6258a7a264bf2859bf8dec73580208fb739374c5
Nils Falke <nfalke@gmail.com>
2016-12-16 15:04:08 +0100

    .

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.get('/sendmail', function (request, response) {
 	var transporter = nodemailer.createTransport({
 			service: 'Gmail',
 			auth: {
-				user: process.env.emailUser,
-				pass: process.env.emailPassword
+				user: process.env.EMAIL_USER,
+				pass: process.env.EMAIL_PWD
 			}
 		});
 
